f128488597a5bfe208aae4c45b4624b2cdc61e5c
Uday Pandey <uday.pandey@gmail.com>
2025-11-11 13:47:18 +0000

    Files updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1272,6 +1272,49 @@ const IndexPage = ({ data }) => {
         </>
       )}
 
+      {showAnswersForDevelopersPhaseThree() && (
+        <>
+          <h2 style={h2HeadingStyles}>
+            Answers - Developer - Phase 3 (G3 -> G4)
+          </h2>
+
+          <h3 style={headingStyles}>
+            Answers - Phase 3 - TopicPapers - RAG - PRPCR
+          </h3>
+
+          <ul style={listStyles}>
+            {phaseThreePrpcrNodes(data).map(({ node }) => (
+              <li key={node.id} style={{ ...listItemStyles, color: "#8EB814" }}>
+                <span>
+                  <Link style={linkStyle} to={node.frontmatter.slug}>
+                    {node.frontmatter.title}{" "}
+                  </Link>
+                </span>
+
+                <span>({node.frontmatter.questions_count})</span>
+              </li>
+            ))}
+          </ul>
+
+          <h3 style={headingStyles}>
+            Answers - Phase 3 - TopicPapers - RAG - PRCR
+          </h3>
+
+          <ul style={listStyles}>
+            {phaseThreePrcrNodes(data).map(({ node }) => (
+              <li key={node.id} style={{ ...listItemStyles, color: "#8EB814" }}>
+                <span>
+                  <Link style={linkStyle} to={node.frontmatter.slug}>
+                    {node.frontmatter.title}{" "}
+                  </Link>
+                </span>
+                <span>({node.frontmatter.questions_count})</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+
       {showAnswersForReviewersPhaseTwo() && (
         <>
           <h2 style={h2HeadingStyles}>
